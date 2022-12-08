fbd94ba5fde239e570d10b8f7ff1315536558eb7
JustMeCodes (a.k.a. JustMe, ThatsJustMe) <92302738+JustMeCodes@users.noreply.github.com>
2022-12-08 20:59:08 +0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace ScreenCut
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dSaveScreenshot = new System.Windows.Forms.SaveFileDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.lbDraw = new System.Windows.Forms.ListBox();
+            this.pDrawSettings = new System.Windows.Forms.Panel();
+            this.bCopy = new System.Windows.Forms.Button();
+            this.bSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nWidth = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbText = new System.Windows.Forms.TextBox();
+            this.lSize = new System.Windows.Forms.Label();
+            this.pDrawSettings.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nWidth)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dSaveScreenshot
+            // 
+            this.dSaveScreenshot.DefaultExt = "png";
+            this.dSaveScreenshot.FileName = "Screenshot.png";
+            // 
+            // lbDraw
+            // 
+            this.lbDraw.FormattingEnabled = true;
+            this.lbDraw.ItemHeight = 16;
+            this.lbDraw.Items.AddRange(new object[] {
+            "Draw",
+            "Line",
+            "Text"});
+            this.lbDraw.Location = new System.Drawing.Point(5, 87);
+            this.lbDraw.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lbDraw.Name = "lbDraw";
+            this.lbDraw.Size = new System.Drawing.Size(52, 52);
+            this.lbDraw.TabIndex = 0;
+            this.lbDraw.SelectedIndexChanged += new System.EventHandler(this.lbDraw_SelectedIndexChanged);
+            // 
+            // pDrawSettings
+            // 
+            this.pDrawSettings.BackColor = System.Drawing.Color.Transparent;
+            this.pDrawSettings.Controls.Add(this.bCopy);
+            this.pDrawSettings.Controls.Add(this.bSave);
+            this.pDrawSettings.Controls.Add(this.label1);
+            this.pDrawSettings.Controls.Add(this.nWidth);
+            this.pDrawSettings.Controls.Add(this.button1);
+            this.pDrawSettings.Controls.Add(this.lbDraw);
+            this.pDrawSettings.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.pDrawSettings.Location = new System.Drawing.Point(52, 50);
+            this.pDrawSettings.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pDrawSettings.Name = "pDrawSettings";
+            this.pDrawSettings.Size = new System.Drawing.Size(160, 324);
+            this.pDrawSettings.TabIndex = 1;
+            this.pDrawSettings.Visible = false;
+            // 
+            // bCopy
+            // 
+            this.bCopy.Location = new System.Drawing.Point(5, 183);
+            this.bCopy.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bCopy.Name = "bCopy";
+            this.bCopy.Size = new System.Drawing.Size(92, 28);
+            this.bCopy.TabIndex = 5;
+            this.bCopy.Text = "Copy";
+            this.bCopy.UseVisualStyleBackColor = true;
+            this.bCopy.Click += new System.EventHandler(this.bCopy_Click);
+            // 
+            // bSave
+            // 
+            this.bSave.Location = new System.Drawing.Point(5, 148);
+            this.bSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bSave.Name = "bSave";
+            this.bSave.Size = new System.Drawing.Size(92, 28);
+            this.bSave.TabIndex = 4;
+            this.bSave.Text = "Save";
+            this.bSave.UseVisualStyleBackColor = true;
+            this.bSave.Click += new System.EventHandler(this.bSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(1, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Width:";
+            // 
+            // nWidth
+            // 
+            this.nWidth.Location = new System.Drawing.Point(5, 55);
+            this.nWidth.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.nWidth.Name = "nWidth";
+            this.nWidth.Size = new System.Drawing.Size(53, 22);
+            this.nWidth.TabIndex = 2;
+            this.nWidth.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.nWidth.ValueChanged += new System.EventHandler(this.nWidth_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(5, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(55, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Color";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbText
+            // 
+            this.tbText.Location = new System.Drawing.Point(17, 16);
+            this.tbText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbText.Name = "tbText";
+            this.tbText.Size = new System.Drawing.Size(505, 22);
+            this.tbText.TabIndex = 2;
+            this.tbText.Visible = false;
+            this.tbText.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbText_KeyDown);
+            // 
+            // lSize
+            // 
+            this.lSize.AutoSize = true;
+            this.lSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lSize.ForeColor = System.Drawing.SystemColors.Control;
+            this.lSize.Location = new System.Drawing.Point(273, 86);
+            this.lSize.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lSize.Name = "lSize";
+            this.lSize.Size = new System.Drawing.Size(52, 25);
+            this.lSize.TabIndex = 3;
+            this.lSize.Text = "XxY";
+            this.lSize.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(689, 508);
+            this.Controls.Add(this.lSize);
+            this.Controls.Add(this.tbText);
+            this.Controls.Add(this.pDrawSettings);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.pDrawSettings.ResumeLayout(false);
+            this.pDrawSettings.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nWidth)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SaveFileDialog dSaveScreenshot;
+        private System.Windows.Forms.ListBox lbDraw;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Panel pDrawSettings;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nWidth;
+        private System.Windows.Forms.TextBox tbText;
+        private System.Windows.Forms.Button bCopy;
+        private System.Windows.Forms.Button bSave;
+        private System.Windows.Forms.Label lSize;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,417 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace ScreenCut
+{
+    public partial class Form1 : Form
+    {
+        // TODO: fix multiple screen support
+        // TODO: make the code cleaner
+        // TODO: allow the selection to be repositionable, size needs to be cropped if went out of screen
+        // TODO: First reposition and drawing after selecting drawing
+        // TODO: come up with a good name for app
+        // TODO: eyedropper for choosing color
+        // TODO: size changable from corners and middle of dimensions
+        // TODO: make the displayed size not go out of the screen
+        // TODO: fix jittering around toolbar when the app is painted
+
+        // TODO: icon for each draw tool
+        // TODO: allow selecting draw tool mode like in photoshop with little settings icon
+
+        // TODO: Rectangle tool: stroke mode or fill mode
+        // TODO: censor tool: rectangle mode and custom - several levels of blurriness
+
+        // DLL libraries used to manage hotkeys
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+        const int HotKeyID = 1;
+
+        private bool init = false;
+        private Image screenshot;
+        private ImageAttributes ia;
+
+        private bool selecting;
+        private Point ul_point;
+        private Point dr_point;
+        private Rectangle ssRect;
+        private Pen pen;
+        
+        private Point mousePositionPrev;
+        private Point mousePosition;
+
+        private enum DrawingType
+        {
+            FreeDraw,
+            Line,
+            Text,
+        }
+
+        private DrawingType drawing;
+        private bool isDrawing = false;
+        private Pen drawingPen;
+        private float dpWidth;
+
+        // (used for line and text drawing)
+        private bool updateDraw;
+
+        private Screen currentScreen = Screen.PrimaryScreen;
+        private Point currentScreenOffset;
+        private Size currentScreenSize = Screen.PrimaryScreen.Bounds.Size;
+
+        // Initialize form
+        public Form1()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.UserPaint |
+                ControlStyles.OptimizedDoubleBuffer, true);
+            UpdateStyles();
+
+            RegisterHotKey(Handle, HotKeyID, 0, (int)Keys.F6);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x0312 && m.WParam.ToInt32() == HotKeyID && !Visible)
+            {
+                currentScreenSize = Screen.FromPoint(Point.Empty).Bounds.Size;
+                foreach (Screen s in Screen.AllScreens)
+                {
+                    if (s.Bounds.Contains(MousePosition))
+                    {
+                        currentScreenOffset = new Point(s.Bounds.Left, s.Bounds.Top);
+                        break;
+                    }
+                }
+
+                currentScreen = Screen.FromPoint(MousePosition);
+
+                var bounds = currentScreen.Bounds;
+                Size = bounds.Size;
+                Location = currentScreen.Bounds.Location;
+                MakeScreenshot();
+                Visible = true;
+            }
+            base.WndProc(ref m);
+        }
+
+        // Initialize program..
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pen = new Pen(Color.White)
+            {
+                Width = 2
+            };
+
+            UpdateDrawingPen(Color.Red, 3);
+
+            mousePosition = mousePositionPrev = new Point();
+
+            foreach (Control c in pDrawSettings.Controls)
+            {
+                c.KeyDown += Form1_KeyDown;
+            }
+
+            Timer timer = new Timer();
+            timer.Interval = 10;
+            timer.Tick += new EventHandler(MouseMoveEvent);
+            timer.Start();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            // Make screenshot and make
+            // the form invisible when opened
+            // (because it doesn't work in Form1_Load
+            // or form constructor)
+            if (!init)
+            {
+                init = true;
+                HideForm();
+                MakeScreenshot();
+
+                ColorMatrix m = new ColorMatrix();
+                m.Matrix33 = 0.5f;
+                ia = new ImageAttributes();
+                ia.SetColorMatrix(m, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
+            }
+
+            // Draw stuff from user
+            if (isDrawing)
+            {
+                Point mouse = MousePosition;
+                using (var gr = Graphics.FromImage(screenshot))
+                    switch (drawing)
+                    {
+                        case DrawingType.FreeDraw:
+                            gr.DrawLine(drawingPen, mousePositionPrev, mouse);
+                            mousePositionPrev = mouse;
+                            break;
+                        case DrawingType.Line:
+                            if (updateDraw)
+                            {
+                                gr.DrawLine(drawingPen, mousePositionPrev, mouse);
+                                mousePositionPrev = mouse;
+                                updateDraw = false;
+                            }
+                            break;
+                        case DrawingType.Text:
+                            if (updateDraw)
+                                gr.DrawString(tbText.Text, tbText.Font, drawingPen.Brush, new PointF(tbText.Left, tbText.Top));
+                            isDrawing = false;
+                            break;
+                        default:
+                            break;
+                    }
+            }
+
+            // Draw screenshot
+            var g = e.Graphics;
+            g.DrawImage(screenshot, new Rectangle(0, 0, currentScreen.Bounds.Width, currentScreen.Bounds.Height),
+                0, 0, screenshot.Width, screenshot.Height, GraphicsUnit.Pixel, ia);
+
+            g.DrawImage(screenshot, ssRect, ssRect, GraphicsUnit.Pixel);
+
+            if (isDrawing && drawing == DrawingType.Line)
+                g.DrawLine(drawingPen, mousePositionPrev, mousePosition);
+
+            g.DrawRectangle(pen, ssRect);
+        }
+        
+        private void MouseMoveEvent(object sender, EventArgs e)
+        {
+            mousePosition = MousePosition;
+
+            if (selecting)
+            {
+                dr_point = MousePosition;
+                UpdateSSRect();
+                Refresh();
+            }
+            if (isDrawing)
+                Refresh();
+
+            updateDraw = false;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (ssRect.IntersectsWith(new Rectangle(e.Location,new Size(1,1))))
+            {
+                if (!isDrawing)
+                {
+                    // Initialize drawing
+                    isDrawing = true;
+                    mousePositionPrev = mousePosition;
+                    switch (drawing)
+                    {
+                        case DrawingType.Line:
+                            updateDraw = true;
+                            break;
+                        case DrawingType.Text:
+                            tbText.Visible = true;
+                            tbText.Left = mousePosition.X;
+                            tbText.Top = mousePosition.Y - 4;
+                            tbText.Focus();
+                            tbText.Height = (int)dpWidth;
+                            tbText.Font = new Font(tbText.Font.FontFamily, dpWidth);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    if (drawing == DrawingType.Line)
+                    {
+                        updateDraw = true;
+                        Refresh();
+                        mousePositionPrev = mousePosition;
+                    }
+                }
+            }
+            else // if (!new Rectangle(e.Location, new Size(1, 1)).IntersectsWith(pDrawSettings.ClientRectangle))
+            {
+                selecting = true;
+                ul_point = e.Location;
+                pDrawSettings.Visible = false;
+                lSize.Visible = true;
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (selecting)
+            {
+                dr_point = e.Location;
+                UpdateSSRect();
+
+                pDrawSettings.Left = ssRect.Right + 4;
+                pDrawSettings.Top = ssRect.Top;
+                pDrawSettings.Visible = true;
+
+                Refresh();
+
+                selecting = false;
+            }
+            else
+            {
+                if (isDrawing)
+                {
+                    if (drawing == DrawingType.FreeDraw)
+                        isDrawing = false;
+                }
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // "Close" the app if Escape was pressed
+            if (e.KeyCode == Keys.Escape)
+                HideForm();
+            // Save screenshot if Enter was pressed
+            else if (e.KeyCode == Keys.Enter)
+                bSave_Click(null, null);
+            // Copy screenshot to clipboard if CTRL+C were pressed
+            else if (e.KeyCode == Keys.C && e.Modifiers == Keys.Control)
+                bCopy_Click(null, null);
+            else if (e.KeyCode == Keys.A && e.Modifiers == Keys.Control)
+            {
+                ssRect = Screen.PrimaryScreen.Bounds;
+                lSize.Top = -lSize.Height - 5;
+                Refresh();
+            }
+        }
+
+        private void tbText_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                tbText.Visible = false;
+                tbText.Text = "";
+            }
+            else if (e.KeyCode == Keys.Enter)
+            {
+                isDrawing = true;
+                updateDraw = true;
+                Refresh();
+                tbText.Visible = false;
+                tbText.Text = "";
+            }
+        }
+
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            if (isDrawing && drawing == DrawingType.Line)
+                isDrawing = false;
+        }
+
+        private void MakeScreenshot()
+        {
+            screenshot = new Bitmap(currentScreenSize.Width, currentScreenSize.Height);
+            using (var sg = Graphics.FromImage(screenshot))
+                sg.CopyFromScreen(currentScreenOffset, Point.Empty, currentScreenSize);
+        }
+
+        private void UpdateSSRect()
+        {
+            ssRect = new Rectangle(
+                ul_point.X, ul_point.Y,
+                dr_point.X - ul_point.X, dr_point.Y - ul_point.Y
+            );
+
+            lSize.Text = $"{Math.Abs(ssRect.Width)}x{Math.Abs(ssRect.Height)}";
+
+            if (ssRect.Width < 0)
+            {
+                ssRect.X = ssRect.X + ssRect.Width;
+                ssRect.Width = -ssRect.Width;
+                lSize.Left = ssRect.Left + 10;
+            }
+            else
+                lSize.Left = ssRect.Right - lSize.Width;
+
+            if (ssRect.Height < 0)
+            {
+                ssRect.Y = ssRect.Y + ssRect.Height;
+                ssRect.Height = -ssRect.Height;
+                lSize.Top = ssRect.Y - lSize.Height - 2;
+            }
+            else
+                lSize.Top = ssRect.Bottom + 2;
+        }
+
+        private void UpdateDrawingPen(Color color, float width)
+        {
+            drawingPen = new Pen(color);
+            drawingPen.Width = width;
+            dpWidth = width;
+        }
+
+        private Image GetClippedScreenshot()
+        {
+            Image s = new Bitmap(ssRect.Width, ssRect.Height);
+            using (var g = Graphics.FromImage(s))
+                g.DrawImage(screenshot, new Rectangle(new Point(0, 0), s.Size), ssRect, GraphicsUnit.Pixel);
+            return s;
+        }
+
+        private void HideForm()
+        {
+            Visible = false;
+            pDrawSettings.Visible = false;
+            isDrawing = false;
+            ssRect = new Rectangle(0, 0, 0, 0);
+            lbDraw.SelectedIndex = -1;
+            lSize.Visible = false;
+        }
+
+        private void lbDraw_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            drawing = (DrawingType)lbDraw.SelectedIndex;
+            isDrawing = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                UpdateDrawingPen(colorDialog1.Color, dpWidth);
+            }
+        }
+
+        private void nWidth_ValueChanged(object sender, EventArgs e)
+        {
+            UpdateDrawingPen(drawingPen.Color, (float)nWidth.Value);
+        }
+
+        private void bSave_Click(object sender, EventArgs e)
+        {
+            if (dSaveScreenshot.ShowDialog() == DialogResult.OK)
+            {
+                GetClippedScreenshot().Save(dSaveScreenshot.FileName);
+                HideForm();
+            }
+        }
+
+        private void bCopy_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Clipboard.SetImage(GetClippedScreenshot());
+            }
+            catch (ExternalException)
+            {
+                MessageBox.Show(
+                    "Can't copy the screenshot at the moment because clipboard is being used by another process. Try again later."
+                    );
+            }
+            HideForm();
+        }
+    }
+}
